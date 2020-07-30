8af0bbeed5dbdc3f1d99c34d706839dd8618c3ee
takashikoyano <koyanotakashi071515@gmail.com>
2020-07-31 04:45:59 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,13 @@
 class MessagesController < ApplicationController
+  
   def index
-     @message = Message.new
-     @room = Room.find(params[:room_id])
-   end
- 
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
+
+
+
    def create
      @room = Room.find(params[:room_id])
      @message = @room.messages.new(message_params)
